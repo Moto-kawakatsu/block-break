014140433db1891961858a7c91148c1c34509488
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-11-30 21:13:10 +0900

    地面に落ちるとゲームオーバーになる実装

--- main.js
+++ main.js
@@ -55,8 +55,18 @@ function draw() {
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
-    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+    if(y + dy < ballRadius) {
         dy = -dy;
+    }
+    else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+            clearInterval(interval); // Needed for Chrome to end game
+        }
     }
     
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
@@ -70,4 +80,4 @@ function draw() {
     y += dy;
 }
 
-setInterval(draw, 10);
+let interval = setInterval(draw, 10);
